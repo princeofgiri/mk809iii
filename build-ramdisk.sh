2456fb8e8eea12008dd9a7d4c6d41596c77b8104
ahmad haris <haris@air>
2015-03-15 09:01:39 +0700

    fixing wrong command

--- build-ramdisk.sh
+++ build-ramdisk.sh
@@ -7,8 +7,8 @@ sudo qemu-debootstrap --no-check-gpg --arch armhf $DISTRO devrootfs $DEBREPO /us
 fi
 echo "deb $DEBREPO $DISTRO main" > sources.list
 sudo mv sources.list devrootfs/etc/apt
-sudo chroot devrootfs apt update 
-sudo chroot devrootfs apt install fbset busybox-static
+sudo chroot devrootfs apt-get update 
+sudo chroot devrootfs apt-get install fbset busybox-static
 mkdir -p ramdisk/lib/arm-linux-gnueabihf/
 mkdir -p ramdisk/bin
 cp devrootfs/lib/ld-linux-armhf.so.3 ramdisk/lib/
